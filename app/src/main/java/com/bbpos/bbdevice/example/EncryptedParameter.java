c4cc52c5489d8cfa8a33a3a06c91489872fb5498
Anyeli Rodriguez <rodriguezanyeli2008@gmail.com>
2017-06-15 08:47:14 -0400

    Creacion de Clase:           - Se crea la clase "EncryptedParameter.java" la cual sera usada para      almacenar los campos de entrada requeridos para llamar una transacción.

---
+++ app/src/main/java/com/bbpos/bbdevice/example/EncryptedParameter.java
@@ -0,0 +1,11 @@
+package com.bbpos.bbdevice.example;
+
+/**
+ * Created by anyeli on 15/06/17.
+ */
+
+class EncryptedParameter {
+    String name="";
+    Class type;
+    String encryptedString="";
+}
