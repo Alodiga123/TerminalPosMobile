12e573df55e3f38edb66ae406a4e1cc10a617e5a
Anyeli Rodriguez <rodriguezanyeli2008@gmail.com>
2017-06-15 11:14:51 -0400

    Activity  "ConfigurationTestActivity" contendra controlador del test de configuración

---
+++ app/src/main/java/com/bbpos/bbdevice/example/ConfigurationTestActivity.java
@@ -0,0 +1,20 @@
+package com.bbpos.bbdevice.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anyeli on 15/06/17.
+ */
+public class ConfigurationTestActivity  extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+}
