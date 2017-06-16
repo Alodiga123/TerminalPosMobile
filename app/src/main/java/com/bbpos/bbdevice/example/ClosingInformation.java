74815ef135088356eae7bfeb43678f2ac881de4e
Anyeli Rodriguez <rodriguezanyeli2008@gmail.com>
2017-06-16 15:18:29 -0400

    - Activity "ClosingInformation":                                   a.- Se añade la constante  "CHARACTER_SEPARATOR" que contiene el separador usado para separar la cadena con la respuesta del servicio.                                   b.-Se elimina el bloque de declaración de variables y se asignan directamente los valores a los txt correspondientes.

--- app/src/main/java/com/bbpos/bbdevice/example/ClosingInformation.java
+++ app/src/main/java/com/bbpos/bbdevice/example/ClosingInformation.java
@@ -8,40 +8,40 @@ import android.widget.TextView;
  * Created by anyeli on 15/06/17.
  */
 
-public class ClosingInformation extends Activity {
+public class ClosingInformation extends Activity
+{
+
+    private static final String CHARACTER_SEPARATOR = ";";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closing_information);
 
-        String parametrosDeCierre;
-        parametrosDeCierre = getIntent().getStringExtra("parametros");
+        String parametrosDeCierre = getIntent().getStringExtra("parametros");
 
-        String[] informacionCierre = parametrosDeCierre.split(",");
+        if(parametrosDeCierre != null && parametrosDeCierre != " " )
+        {
+            String[] closingInformation = parametrosDeCierre.split(CHARACTER_SEPARATOR);
 
-        String hora               = informacionCierre[1];
-        String fecha              = informacionCierre[1];
-        String lote               = informacionCierre[2];
-        String totalAprobado      = informacionCierre[3];
-        String totalTransEnviadas = informacionCierre[4];
-        String montoTotalCierre   = informacionCierre[5];
+            int totalTransactionsSuccess =  Integer.parseInt(closingInformation[9].substring(16));
+            int totalTransactionsCancel = Integer.parseInt(closingInformation[10].substring(17));
+            String totalTransactionsSent = String.valueOf(totalTransactionsSuccess + totalTransactionsCancel);
 
+            TextView txtHOUR             = (TextView) findViewById(R.id.txtHour);
+            TextView txtDATE             = (TextView) findViewById(R.id.txtDate);
+            TextView txtLOT              = (TextView) findViewById(R.id.txtLot);
+            TextView txtTotalApproved    = (TextView) findViewById(R.id.txtTotalApproved);
+            TextView txtSentTransactions = (TextView) findViewById(R.id.txtSentTransactions);
+            TextView txtClosingAmount    = (TextView) findViewById(R.id.txtClosingAmount);
 
-        TextView txtHORA               = (TextView) findViewById(R.id.txtHora);
-        TextView txtFECHA              = (TextView) findViewById(R.id.txtFecha);
-        TextView txtLOTE               = (TextView) findViewById(R.id.txtLote);
-        TextView txtTotalAprobado      = (TextView) findViewById(R.id.txtAprobado);
-        TextView txtTotalTransEnviadas = (TextView) findViewById(R.id.txtTransaccionEnviada);
-        TextView txtmontoTotalCierre   = (TextView) findViewById(R.id.txtMontoCierre);
-
-        txtHORA.setText(hora);
-        txtFECHA.setText(fecha);
-        txtLOTE.setText(lote);
-        txtTotalAprobado.setText(totalAprobado);
-        txtTotalTransEnviadas.setText(totalTransEnviadas);
-        txtmontoTotalCierre.setText(montoTotalCierre);
-
+            txtHOUR.setText(closingInformation[3].substring(22,30));
+            txtDATE.setText(closingInformation[3].substring(11,21));
+            txtLOT.setText(closingInformation[5].substring(7,9));
+            txtTotalApproved.setText(closingInformation[10].substring(17));
+            txtSentTransactions.setText(totalTransactionsSent);
+            txtClosingAmount.setText(closingInformation[14].substring(16));
+        }
     }
-
 }
