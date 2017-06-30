4b575fa737e3e7ff626e2f32bd71e8837e25d58c
Anyeli Rodriguez <rodriguezanyeli2008@gmail.com>
2017-06-30 16:52:55 -0400

    Se crea clase ErrorMessages

---
+++ app/src/main/java/com/bbpos/bbdevice/example/ErrorMessages.java
@@ -0,0 +1,13 @@
+package com.bbpos.bbdevice.example;
+
+/**
+ * Created by anyeli on 29/06/17.
+ */
+
+public  class ErrorMessages {
+
+    public static final String SUCCESSFUL_OPERATION = "0";
+    public static final String TERMINAL_OLD_PASSWORD_NOT_FOUND = "7";
+    public static final String TERMINAL_REPEATED_PASSWORD = "8";
+
+}
