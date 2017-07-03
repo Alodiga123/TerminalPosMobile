06ea62767602cc0488b8b09f849b0c42a3750600
Anyeli Rodriguez <rodriguezanyeli2008@gmail.com>
2017-07-03 11:54:18 -0400

    Se añade Clase "SuccessfulKeyChange.java"

---
+++ app/src/main/java/com/bbpos/bbdevice/example/SuccessfulKeyChange.java
@@ -0,0 +1,21 @@
+package com.bbpos.bbdevice.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by anyeli on 03/07/17.
+ */
+
+public class SuccessfulKeyChange extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_successful_key_change);
+
+
+    }
+}
